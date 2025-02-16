5893b4793aea09059c54e3cc1760c1f872117f9d
Hojei Hsu <hhhhojeihsu@gmail.com>
2025-02-17 00:23:49 +0800

    Set Icon

--- steamCloudSaveDownloaderGUI/__main__.py
+++ steamCloudSaveDownloaderGUI/__main__.py
@@ -1,9 +1,11 @@
+import os
 import sys
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtGui
 from . import main_window
 from .core import core
 from . import data_provider
 from .steamCloudSaveDownloader.steamCloudSaveDownloader import logger
+from .res import icon
 
 def __main__():
     core.init()
@@ -12,6 +14,7 @@ def __main__():
     logger.logger.info("scsd-gui started")
 
     app = QtWidgets.QApplication([])
+    app.setWindowIcon(QtGui.QIcon(QtGui.QPixmap(":/scsd_256.jpg")))
 
     window = main_window.main_window()
     window.resize(800, 600)
