d5ebc382620412f5f790025f62764d7b37d1165c
Hojei Hsu <hhhhojeihsu@gmail.com>
2025-03-13 23:18:12 +0800

    Set Version Number if no Tag

--- scripts/build/windows/set_version_from_template.ps1
+++ scripts/build/windows/set_version_from_template.ps1
@@ -1,17 +1,16 @@
 # v1.2.3\n
 $version_raw = Get-Content ./version.txt -Raw
 
-$version = $version_raw -replace "`n",""  -replace "`r","" -replace "v",""
-# 1.2.3
+if ($version_raw -Match "v") {
+    $version = $version_raw -replace "`n",""  -replace "`r","" -replace "v",""
+    # 1.2.3
 
-$first_ver, $second_ver, $third_ver = $version.split('.')
-
-#DBG
-Write-Host $version_raw
-Write-Host $version
-Write-Host $first_ver
-Write-Host $second_ver
-Write-Host $third_ver
+    $first_ver, $second_ver, $third_ver = $version.split('.')
+} else {
+    $first_ver = "0"
+    $second_ver = "0"
+    $third_ver = "0"
+}
 
 
 $version_template = Get-Content ./scripts/build/windows/version_template.txt -Raw
@@ -21,7 +20,3 @@ $version_template = $version_template -replace "ACHAKA2","($($first_ver), $($sec
 $version_template = $version_template -replace "ACHAKA3","v$($first_ver).$($second_ver).$($third_ver)"
 $version_template = $version_template -replace "ACHAKA4","v$($first_ver).$($second_ver).$($third_ver)"
 $version_template | Out-File -Filepath ./version_windows.txt -Encoding "UTF8"
-
-#DBG
-$a = Get-Content ./version_windows.txt -Raw
-Write-Host $a
