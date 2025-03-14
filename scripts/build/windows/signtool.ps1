369b8fcad58d854582c01b8ae92dcbe64b3803b5
Hojei Hsu <hhhhojeihsu@gmail.com>
2025-03-14 22:24:55 +0800

    Add Signtool

--- scripts/build/windows/signtool.ps1
+++ scripts/build/windows/signtool.ps1
@@ -2,16 +2,33 @@
 
 $dir = dir "C:/Program Files (x86)/Windows Kits/10/bin/" | ?{$_.PSISContainer}
 
+$newest_version = 0
+$newest_version_path = ""
 foreach ($d in $dir) {
-    Write-Host $d
-    Write-Host $d.FullName
-    #if (-Not ($d.FullName.EndsWith('.0'))) {
-    #    Write-Host "  continue"
-    #    continue
-    #}
+    if (-Not ($d.EndsWith('.0'))) {
+        Write-Host "Skip $($d)"
+        continue
+    }
 
-    #$version =
+    if (-Not (Test-Path "$($d)\x64\signtool.exe" -PathType Leaf)) {
+        Write-Host "[no exe] Skip $($d)"
+        continue
+    }
+
+    $leaf = $d.split("\")[-1]
+    Write-Host "Leaf: $leaf"
+
+    $version = $leaf -replace ".","" -as [int]
+
+    if ($version -gt $newest_version) {
+        $newest_version
+        $newest_version_path = $d
+    }
 }
+
+$signtool = "$($newest_version_path)\x64\signtool.exe"
+
+Write-Host "Signtool: $($signtool)"
 
 #certificate: '${{ secrets.SIGNTOOL_CERT }}'
 #password: '${{ secrets.SIGNTOOL_PASS }}'
