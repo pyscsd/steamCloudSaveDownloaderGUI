bb1e7775f353b9f0ea374d1b48d69cf6ee21c0d5
hhhhhojeihsu <18467750+hhhhhojeihsu@users.noreply.github.com>
2025-05-04 13:59:28 +0000

    Auto .ts Files Update

---
+++ steamCloudSaveDownloaderGUI/i18n/scsdGUI_zh_CN.ts
@@ -0,0 +1,282 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name></name>
+    <message>
+        <location filename="../game_info_dialog.py" line="24"/>
+        <source>Name/Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game_info_dialog.py" line="25"/>
+        <source>Date Written</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>_save_downloader</name>
+    <message>
+        <location filename="../save_downloader.py" line="84"/>
+        <source>Downlading {game[&apos;name&apos;]} ({count} / {len(p_target_game_list)})</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../save_downloader.py" line="143"/>
+        <source>Downlading {p_game_name}: {p_file_name}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../save_downloader.py" line="147"/>
+        <source>Initializing...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>about_action</name>
+    <message>
+        <location filename="../menu.py" line="251"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>disable_all_action</name>
+    <message>
+        <location filename="../table_widget.py" line="361"/>
+        <source>Disable All</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>download_action</name>
+    <message>
+        <location filename="../menu.py" line="111"/>
+        <source>Download</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../menu.py" line="132"/>
+        <source>Already downloading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../menu.py" line="136"/>
+        <source>Download All Executed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../menu.py" line="139"/>
+        <source>Download Outdated Executed</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>enable_all_action</name>
+    <message>
+        <location filename="../table_widget.py" line="352"/>
+        <source>Enable All</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>exit_dialog</name>
+    <message>
+        <location filename="../main_window.py" line="13"/>
+        <source>Close in progress</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_window.py" line="14"/>
+        <source>Please wait for the program to close gracefully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>game_info_dialog</name>
+    <message>
+        <location filename="../game_info_dialog.py" line="228"/>
+        <source>{game_name} Saves</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>login_action</name>
+    <message>
+        <location filename="../menu.py" line="15"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>logout_action</name>
+    <message>
+        <location filename="../menu.py" line="31"/>
+        <source>Logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>open_saves_directory_action</name>
+    <message>
+        <location filename="../game_info_dialog.py" line="165"/>
+        <source>Open File Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../table_widget.py" line="392"/>
+        <source>Open Saves Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>options_action</name>
+    <message>
+        <location filename="../menu.py" line="75"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>quit_action</name>
+    <message>
+        <location filename="../menu.py" line="262"/>
+        <location filename="../system_tray.py" line="18"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>refresh_action</name>
+    <message>
+        <location filename="../menu.py" line="93"/>
+        <location filename="../menu.py" line="101"/>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>scheduled_downloader_timer</name>
+    <message>
+        <location filename="../menu.py" line="211"/>
+        <location filename="../menu.py" line="245"/>
+        <source>Auto Download ({self.count_down})</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../menu.py" line="217"/>
+        <source>Auto Downloading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../menu.py" line="241"/>
+        <source>Auto Download Disabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>session_menu</name>
+    <message>
+        <location filename="../menu.py" line="48"/>
+        <source>Session</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>show_action</name>
+    <message>
+        <location filename="../system_tray.py" line="8"/>
+        <source>Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>status_bar</name>
+    <message>
+        <location filename="../status_bar.py" line="26"/>
+        <source>Authenticating...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../status_bar.py" line="40"/>
+        <source>Double click to view files. Right click for options.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../status_bar.py" line="49"/>
+        <source>Ready. Press &apos;Refresh&apos; to populate list or &apos;Start&apos; to start downloading.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../status_bar.py" line="52"/>
+        <source>No session. Please create session with Session &gt; Login.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>stop_action</name>
+    <message>
+        <location filename="../menu.py" line="156"/>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>system_tray</name>
+    <message>
+        <location filename="../system_tray.py" line="71"/>
+        <source>scsd-gui is now running in background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../system_tray.py" line="82"/>
+        <source>Download Complete</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>table_model</name>
+    <message>
+        <location filename="../table_widget.py" line="227"/>
+        <source>Loading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../table_widget.py" line="229"/>
+        <source>N/A</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>table_refresher</name>
+    <message>
+        <location filename="../table_widget.py" line="130"/>
+        <source>Refreshing...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>table_widget</name>
+    <message>
+        <location filename="../table_widget.py" line="519"/>
+        <source>Search App ID / Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>tree_model</name>
+    <message>
+        <location filename="../game_info_dialog.py" line="55"/>
+        <source>Placeholder</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>view_files_action</name>
+    <message>
+        <location filename="../table_widget.py" line="378"/>
+        <source>View Save Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
