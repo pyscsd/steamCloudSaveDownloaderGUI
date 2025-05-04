f953a9204de9f17739666c306380072c2c399918
Hojei Hsu <hhhhojeihsu@gmail.com>
2025-05-04 18:58:59 +0800

    Auto Generate TS Files

---
+++ scripts/build/windows/languages.ps1
@@ -0,0 +1,1 @@
+$languages = "zh_TW", "zh_CN"
---
+++ scripts/build/windows/lupdate.ps1
@@ -0,0 +1,5 @@
+. .\scripts\build\windows\languages.ps1
+foreach ($language in $languages)
+{
+    pyside6-lupdate.exe -extensions py -no-recursive .\steamCloudSaveDownloaderGUI -ts ".\steamCloudSaveDownloaderGUI\i18n\scsdGUI_$($language).ts"
+}
